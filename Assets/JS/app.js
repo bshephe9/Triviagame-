c7e5288a40ac73ef6492afb22f5f574eca7038b6
bshephe9 <brookeshepherd672@gmail.com>
2018-09-18 22:00:53 -0400

    update

--- Assets/JS/app.js
+++ Assets/JS/app.js
@@ -1,136 +1,175 @@
- //This is the array of questions the player will be answering//
- var triviaQuestions = [{
-     question: "Which of the following was a character in A Bug's Life?",
-     answerList:["Dot", "Fineas", "Nala", "Tiana"],
-     answer: 0
- }, {
+ 
+ $(document).ready(function (){
+ 
+ //Trivia Question Array
+ var options = [
+     {
+    question: "Which of the following was a character in A Bug's Life?",
+    choice:["Dot", "Fineas", "Nala", "Tiana"],
+     answer: 0,
+    photo: "Assets/Images/dot.png"
+ },{
      question: "Who is Princess Auroa's Prince?",
-     answerList:["Prince Henry", "Prince Phillip", "Prince Charming", "John Smith"],
-     answer: 1
+     choice:["Prince Henry", "Prince Phillip", "Prince Charming", "John Smith"],
+     answer: 1,
+     photo: "Assets/Images/phillip.jpg"
  }, {
      question: "Who gave Snow White the poison apple?",
-     answerList:["The Bad Queen", "The Evil Witch", "The Evil Queen", "The Wicked Queen"],
-     answer: 2
+     choice:["The Bad Queen", "The Evil Queen", "The Ugly Lady", "The Old Witch"],
+     answer: 1,
+     photo: "Assets/Images/evil.png"
  }, {
      question: "Who helped Nemo find his way back to his dad?",
-     answerList:["Darla", "Gill", "Bruce", "Dory"],
-     answer: 3
+     choice:["Darla", "Gill", "Bruce", "Dory"],
+     answer: 3,
+     photo: "Assets/Images/dory.jpg"
  }, {
      question: "What is the name of the Racoon from Pocahontas?",
-     answerList:["Nico", "Nix", "Meeko", "Meeks"],
-     answer: 2
+     choice:["Nico", "Nix", "Meeko", "Meeks"],
+     answer: 2,
+     photo: "Assets/Images/meeko.jpg"
  }, {
      question: "Who tried to kidnap the Little Mermaid?",
-     answerList:["Ursula, Cruella De Vil", "Maleficent", "Jafar"],
-     answer: 0
+     choice:["Ursula", "Cruella De Vil", "Maleficent", "Jafar"],
+     answer: 0,
+     photo: "Assets/Images/ursula.png"
  }, {
      question: "What did Cinderella lose after the royal ball?",
-     answerList:["A necklace", "A glass slipper", "A piece of her dress", "Her Prince"],
-     answer: 1
- }, {
-     question: "What is written on the bottom of Woody's boot?",
-     answerList:["Sandy", "Danny", "Andy", "Manny"],
-     answer: 2
- }, {
-     question: "What is the name of the Racoon from Pocahontas?",
-     answerList:["Nico", "Nix", "Meeko", "Meeks"],
-     answer: 2
- }, {
-     question: "Who does Cinderella live with?",
-     answerList:["Step Mother", "Dad", "Mother", "Twin Sister"],
-     answer: 0
- }, {
-     question: "What country does Nemo go to in Finding Nemo?",
-     answerList:["Africa", "Greece", "Australia","Spain"],
-     answer : 2
- } , {
-     question: "What does the crocodile swallow in Peter Pan?",
-     answerList:["Peter Pan", "Tinker Bell", "A Clock", "Captain Hook"],
-     answer: 2
- }, {
-     question: "Which Disney movie features the song Two Worlds",
-     answerList:["Aladin", "Tarzan", "A Bug's Life","Beauty and the Beast"],
-     answer: 2
- }]
+     choice:["A necklace", "A glass slipper", "A piece of her dress", "Her Prince"],
+     answer: 1,
+     photo: "Assets/Images/cinderella.jpg"
+ }];
 
- //Array of pictures that will appear after the solution of each question is displayed//
-var picArray = ['question1', 'question 2','question 3', 'question 4', 'question 5', 'question 6', 'questions 7', 'question 8','question 9', 'question 10', 'question 11', 'question 12', 'question 13']; 
+ 
+var correctCount = 0;
+var wrongCount = 0;
+var unanswerCount = 0;
+var timer = 20;
+var intervalId;
+var userGuess ="";
+var running = false;
+var qCount = options.length;
+var pick;
+var index;
+var newArray = [];
+var holder = [];
 
-var currentQuestion; var correctAnswer; var incorrectAnswer; var notAnswered; var seconds; var time; var answered; var playerSelect; 
-var alerts = { 
-    correct: "That's right!",
-    incorrect: "You're wrong, try again!",
-    endTime: "Out of time!",
-    end: "Game Over. Let's see your results!"
+$("#reset").hide();
+//click start button to start game
+$("#start").on("click", function () {
+		$("#start").hide();
+		displayQuestion();
+		runTimer();
+		for(var i = 0; i < options.length; i++) {
+	holder.push(options[i]);
+}
+	})
+//timer start
+function runTimer(){
+	if (!running) {
+	intervalId = setInterval(decrement, 1000); 
+	running = true;
+	}
+}
+//timer countdown
+function decrement() {
+	$("#timeleft").html("<h3>Time remaining: " + timer + "</h3>");
+	timer --;
+
+	//stop timer if reach 0
+	if (timer === 0) {
+		unanswerCount++;
+		stop();
+		$("#answerblock").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+		hidepicture();
+	}	
 }
 
-$("#startButton").on('click',function(){
-    $(this).hide(); 
-    startGame(); 
-}); 
+//Timer Function
+function stop() {
+	running = false;
+	clearInterval(intervalId);
+}
+//randomly pick question in array if not already shown
+//display question and loop though and display possible answers
+function displayQuestion() {
+	
+	index = Math.floor(Math.random()*options.length);
+	pick = options[index];
 
-$("#againButton").on('click',function(){
-    $(this).hide(); 
-    startGame(); 
-});
+	$("#questionblock").html("<h2>" + pick.question + "</h2>");
+		for(var i = 0; i < pick.choice.length; i++) {
+			var userChoice = $("<div>");
+			userChoice.addClass("answerchoice");
+			userChoice.html(pick.choice[i]);
+			
+			userChoice.attr("data-guessvalue", i);
+			$("#answerblock").append(userChoice);
 
-function startGame(){ 
-    $("#lastMessage").empty();
-    $("#correctAnswers").empty();
-    $("#incorrectAnswers").empty();
-    $("#unanswered").empty();
-    currentQuestion = 0; 
-    correctAnswer = 0; 
-    incorrectAnswer = 0; 
-    unanswered = 0;
-    newQuestion();
+		}	
+}
+//click function to select answer and outcomes
+$(".answerchoice").on("click", function () {
+	//grab array position from userGuess
+	userGuess = parseInt($(this).attr("data-guessvalue"));
+
+	//correct guess or wrong guess outcomes
+	if (userGuess === pick.answer) {
+		stop();
+		correctCount++;
+		userGuess="";
+		$("#answerblock").html("<p>Correct!</p>");
+		hidepicture();
+
+	} else {
+		stop();
+		wrongCount++;
+		userGuess="";
+		$("#answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+		hidepicture();
+	}
+})
+
+function hidepicture () {
+	$("#answerblock").append("<img src=" + pick.photo + ">");
+	newArray.push(pick);
+	options.splice(index,1);
+
+	var hidpic = setTimeout(function() {
+		$("#answerblock").empty();
+		timer= 20;
+
+	//run the score screen if all questions answered
+	if ((wrongCount + correctCount + unanswerCount) === qCount) {
+		$("#questionblock").empty();
+		$("#questionblock").html("<h3>Game Over!  Here's how you did: </h3>");
+		$("#answerblock").append("<h4> Correct: " + correctCount + "</h4>" );
+		$("#answerblock").append("<h4> Incorrect: " + wrongCount + "</h4>" );
+		$("#answerblock").append("<h4> Unanswered: " + unanswerCount + "</h4>" );
+		$("#reset").show();
+		correctCount = 0;
+		wrongCount = 0;
+		unanswerCount = 0;
+
+	} else {
+		runTimer();
+		displayQuestion();
+
+	}
+	}, 3000);
+
 }
 
-function newQuestion() {
-    $("#message").empty();
-    $("#correctedAnswer").empty();
-    $("#pic").empty();
-    answered = true; 
-}
+$("#reset").on("click", function() {
+	$("#reset").hide();
+	$("#answerblock").empty();
+	$("#questionblock").empty();
+	for(var i = 0; i < holder.length; i++) {
+		options.push(holder[i]);
+	}
+	runTimer();
+	displayQuestion();
 
-//sets up new questions and answerList
-$("currentQuestion").html("Question #" + (currentQuestion+1)+"/"+triviaQuestions.length); 
-$(".question").html("<h2>"+triviaQuestions[currentQuestion].question + "</h2>");
-for(var i = 0; i < 4; i++) { 
-    var choices = $('div'); 
-    choices.text(triviaQuestions[currentQuestion].answerList[i]); 
-    choices.attr({"data-idex": i}); 
-    choices.addClass("thischoice"); 
-    $(".answerList").append(choices); 
-}
- countdown(); 
- $(".thisChoice").on("click", function() { 
-     userSelect = $(this).data("index"); 
-     clearInterval(time); 
-     answerPage(); 
- }); 
+})
 
-}
-
-function
-
-var time;
-var seconds;
-function countdown() {
-    seconds= 120;
-    $("#timeLeft").text("Time Left:" + seconds);
-    time = setInterval(showCountdown, 1000);
-}
-
-function showCountdown() {
-    seconds--;
-    $('#timeLeft').text('Time Left:' + seconds);
-    if (seconds < 1) {
-        clearInterval(time);
-    }
-}
-
-// function scoreboard() {
-
-// }
-countdown();
+})
